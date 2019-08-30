8c81f652a9a527dbe7acca5087b462dbb3d8b587
Anachronaut <bmhodapp@gmail.com>
2019-08-29 20:19:05 -0500

    Second Commit: added guessthenumber.py

---
+++ guessthenumber.py
@@ -0,0 +1,17 @@
+import random
+
+num = random.randint(1,5)
+
+def numberGuess(num):
+    guess = input("Guess a number between 1 and 5 (Integers Only)")
+    guessint = int(guess)
+    if guessint == num:
+        print("You Win!")
+    elif guessint > num:
+        print("Too high!")
+        numberGuess(num)
+    elif guessint < num:
+        print("Too low!")
+        numberGuess(num)
+
+numberGuess(num)
